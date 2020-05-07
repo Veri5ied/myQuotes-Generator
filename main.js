61a755edafc8e97ab0747a85b271f5c6095e6e61
Veri5ied <43749581+Veri5ied@users.noreply.github.com>
2020-05-08 00:19:23 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const generateQuotes = () => {
+    fetch('https://api.quotable.io/random')
+        .then(function (data) {
+            return data.json();
+        })
+        .then(function (data) {
+            document.getElementById('quote').innerHTML = data.content;
+            document.getElementById('author').innerHTML = "- " + data.author;
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
+}
+
+setInterval(generateQuotes(), 10000);
