a15c7af64148a7cfa805bf2297cad9316f5b5018
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2020-05-08 00:43:54 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,4 @@
-const generateQuotes = () => {
-    fetch('https://api.quotable.io/random')
-        .then(function (data) {
-            return data.json();
-        })
-        .then(function (data) {
-            document.getElementById('quote').innerHTML = data.content;
-            document.getElementById('author').innerHTML = "- " + data.author;
-        })
-        .catch(function (err) {
-            console.log(err);
-        })
-}
 
-setInterval(generateQuotes(), 1000);
 const url = "https://api.quotable.io/random";
 function generateQuote(){
 	fetch(url)
